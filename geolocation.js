50bf1f8587931db353f0d82514c6cd7a99a21b48
Cassie Cannelle <113533171+casscannelle@users.noreply.github.com>
2023-12-21 15:03:13 -0300

    geolocation e notification

--- geolocation.js
+++ geolocation.js
@@ -8,10 +8,9 @@ function showLocation(position){
   const long = position.coords.longitude;
   const lat = position.coords.latitude;
 
-  console.log(`longitude: ${long}, latidute: ${lat}`);
+  const mapUrl = `https://maps.google.com/maps?q=${lat},${long}&t=&z=10&ie=UTF8&iwloc=&output=embed`;
 
-  const url = `https://maps.google.com/maps?q=${lat},${long}&t=&z=10&ie=UTF8&iwloc=&output=embed`;
-
-  window.open(url, 'map');
+  const mapContainer = document.getElementById("map");
+    mapContainer.innerHTML = `<iframe width="600" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="${mapUrl}"></iframe>`;
 
 }
