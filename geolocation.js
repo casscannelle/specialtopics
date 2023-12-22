1db5862d9f64a9fd61f767e12bcae52669ecdab3
Cassie Cannelle <113533171+casscannelle@users.noreply.github.com>
2023-12-22 20:45:24 -0300

    ajustes responsividade geolocation

--- geolocation.js
+++ geolocation.js
@@ -8,12 +8,7 @@ function showLocation(position){
   const long = position.coords.longitude;
   const lat = position.coords.latitude;
 
-  const mapUrl = `https://maps.google.com/maps?q=${lat},${long}&t=&z=10&ie=UTF8&iwloc=&output=embed`;
+  const url = `https://maps.google.com/maps?q=${lat},${long}&t=&z=10&ie=UTF8&iwloc=&output=embed`;
 
-  const mapContainer = document.getElementById("map");
-    mapContainer.innerHTML = `<iframe frameborder="0" height="300vh" width="600vw" scrolling="no" marginheight="0" marginwidth="0" src="${mapUrl}"></iframe>`;
-    mapContainer.setAttribute("allowfullscreen", "");
-    mapContainer.setAttribute("loading", "lazy");
-
-    document.getElementById("map-container").appendChild(mapContainer);
+  window.open(url, 'map');
 }
